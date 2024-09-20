17cf4c8570b7de767804e044eb90ff68729c6be0
parvindar <parvindarsingh43958@gmail.com>
2024-09-21 00:53:09 +0530

    ToDo project

---
+++ 10ToDOwContextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            text:"To do",
+            completed:false,
+        }
+    ],
+    addTodo:() =>{},
+    updateTodo:()=>{},
+    deleteTodo:()=>{},
+    toggleComplete:()=>{},
+})
+
+export const useToDo = () =>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
---
+++ 10ToDOwContextLocal/src/contexts/index.js
@@ -0,0 +1,2 @@
+import { TodoContext,TodoProvider,useToDo } from "./TodoContext";
+export { TodoContext, TodoProvider, useToDo };
