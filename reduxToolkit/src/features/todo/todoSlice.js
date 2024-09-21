82dee2524d6db2363b5f96c215770556c007347f
parvindar <parvindarsingh43958@gmail.com>
2024-09-21 18:18:45 +0530

    To do with redux toolkit

---
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,36 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+// const initialState = () =>{
+//    todos:[{id:1, text:"To do"}]
+// }
+
+export const todoSlice = createSlice({
+    name:"todo",
+    initialState:{todos:[{id:1, text:"To do",completed:true}]},
+    reducers:{
+        addTodo:(state, action)=>{
+            const todo = {
+                id:nanoid(), 
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((todo)=>{
+                todo.id !== action.payload.id
+            })
+        },
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => 
+                todo.id === action.payload.id 
+                    ? { ...todo, completed: action.payload.completed } 
+                    : todo
+            );
+        }
+        
+    }
+})
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
